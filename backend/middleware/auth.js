5becb5e46ce81f7e4e3cb4ca214119a63b415df7
Camille BOUCHACOT <110158288+camillebouchacot@users.noreply.github.com>
2022-12-02 15:34:31 +0100

    Add files via upload

---
+++ backend/middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    try {
+        //on split le bearer du token pour ensuite récupérer le deuxième élément de cet array qui est le token
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        const userId = decodedToken.userId;
+        if (req.body.userId && req.body.userId !== userId) {
+            throw 'User ID non valable!';
+        } else {
+            next();
+        }
+    }
+    catch {
+        res.status(401).json({
+            error: new Error('Requête non authentifiée')
+        });
+    }
+};
